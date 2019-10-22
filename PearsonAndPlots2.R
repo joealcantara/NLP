69982e365b0a53d96e846f27ff86eee5315f5049
Joe Alcantara <joe@joealcantara.com>
2019-10-22 09:05:57 +0100

    R script

--- PearsonAndPlots2.R
+++ PearsonAndPlots2.R
@@ -17,19 +17,19 @@ dataReagan = read.csv("~/Documents/NLP/Reagan.csv")
 dataTrump = read.csv("~/Documents/NLP/Trump.csv")
 
 dfReagan = data.frame(feature = character(),
-                r2      = numeric(),
-                pvalue  = numeric(),
-                stringsAsFactors = FALSE)
+                      r2      = numeric(),
+                      pvalue  = numeric(),
+                      stringsAsFactors = FALSE)
 
 dfBush = data.frame(feature = character(),
-                      r2      = numeric(),
-                      pvalue  = numeric(),
-                      stringsAsFactors = FALSE)
+                    r2      = numeric(),
+                    pvalue  = numeric(),
+                    stringsAsFactors = FALSE)
 
 dfTrump = data.frame(feature = character(),
-                      r2      = numeric(),
-                      pvalue  = numeric(),
-                      stringsAsFactors = FALSE)
+                     r2      = numeric(),
+                     pvalue  = numeric(),
+                     stringsAsFactors = FALSE)
 
 dataBush$Julian = NULL
 dataTrump$Julian = NULL
@@ -46,20 +46,20 @@ for(i in seq_along(names(dataReagan))){
   if(is.numeric(dataReagan[, i])){
     pearson = cor.test(dataReagan$Days, dataReagan[, i], method = "pearson")
     dfReagan = rbind(dfReagan, data.frame(feature = names(dataReagan)[i],
-                              r2      = unname(pearson$estimate),
-                              pvalue  = pearson$p.value,
-                              stringsAsFactors = FALSE))
-    }
+                                          r2      = unname(pearson$estimate),
+                                          pvalue  = pearson$p.value,
+                                          stringsAsFactors = FALSE))
   }
+}
 
 
 for(i in seq_along(names(dataBush))){
   if(is.numeric(dataBush[, i])){
     pearson = cor.test(dataBush$Days, dataBush[, i], method = "pearson")
     dfBush = rbind(dfBush, data.frame(feature = names(dataBush)[i],
-                              r2      = unname(pearson$estimate),
-                              pvalue  = pearson$p.value,
-                              stringsAsFactors = FALSE))
+                                      r2      = unname(pearson$estimate),
+                                      pvalue  = pearson$p.value,
+                                      stringsAsFactors = FALSE))
   }
 }
 
@@ -67,9 +67,9 @@ for(i in seq_along(names(dataTrump))){
   if(is.numeric(dataTrump[, i])){
     pearson = cor.test(dataTrump$Days, dataTrump[, i], method = "pearson")
     dfTrump = rbind(dfTrump, data.frame(feature = names(dataTrump)[i],
-                              r2      = unname(pearson$estimate),
-                              pvalue  = pearson$p.value,
-                              stringsAsFactors = FALSE))
+                                        r2      = unname(pearson$estimate),
+                                        pvalue  = pearson$p.value,
+                                        stringsAsFactors = FALSE))
   }
 }
 
@@ -150,53 +150,42 @@ rm(subsetTrump2)
 scatter_plot <- ggplot(dataReagan, aes(Days, ppron))
 scatter_plot + geom_point() + labs(x = "Days", y = "ppron") + geom_smooth(method="lm")
 
-jpeg("~/Documents/NLP/plots/RRppron.jpg", width = 350, height = 350)
 scatter_plot <- ggplot(dataReagan, aes(Days, ppron))
 scatter_plot + geom_point() + labs(x = "Days", y = "ppron") + geom_smooth(method="loess")
 cor.test(dataReagan$Days, dataReagan$ppron, method = "pearson", conf.level = 0.95)
-dev.off()
 
 scatter_plot <- ggplot(dataBush, aes(Days, ppron))
 scatter_plot + geom_point() + labs(x = "Days", y = "ppron") + geom_smooth(method="lm")
-#jpeg("~/Documents/NLP/plots/GHWBppron.jpg", width = 350, height = 350)
 scatter_plot <- ggplot(dataBush, aes(Days, ppron))
 scatter_plot + geom_point() + labs(x = "Days", y = "ppron") + geom_smooth(method="loess")
 cor.test(dataBush$Days, dataBush$ppron, method = "pearson", conf.level = 0.95)
-#dev.off()
 
 scatter_plot <- ggplot(dataReagan, aes(Days, UniqueWords))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWords") + geom_smooth(method="lm")
-#jpeg("~/Documents/NLP/plots/RRUniqueWords.jpg", width = 350, height = 350)
 scatter_plot <- ggplot(dataReagan, aes(Days, UniqueWords))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWords") + geom_smooth(method="loess")
 cor.test(dataReagan$Days, dataReagan$UniqueWords, method = "pearson", conf.level = 0.95)
-#dev.off()
 
 scatter_plot <- ggplot(dataBush, aes(Days, UniqueWords))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWords") + geom_smooth(method="lm")
-#jpeg("~/Documents/NLP/plots/GHWBUniqueWords.jpg", width = 350, height = 350)
+
 scatter_plot <- ggplot(dataBush, aes(Days, UniqueWords))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWords") + geom_smooth(method="loess")
 cor.test(dataBush$Days, dataBush$UniqueWords, method = "pearson", conf.level = 0.95)
-#dev.off()
-jpeg("~/Documents/NLP/plots/RRUniqueWordsNormalisedLM.jpg", width = 350, height = 350)
+
 scatter_plot <- ggplot(dataReagan, aes(Days, UniqueWordsNormalised))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWordsNormalised") + geom_smooth(method="lm")
-dev.off()
-jpeg("~/Documents/NLP/plots/RRUniqueWordsNormalisedLOESS.jpg", width = 350, height = 350)
+
 scatter_plot <- ggplot(dataReagan, aes(Days, UniqueWordsNormalised))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWordsNormalised") + geom_smooth(method="loess")
 cor.test(dataReagan$Days, dataReagan$UniqueWords, method = "pearson", conf.level = 0.95)
-dev.off()
+
 
 scatter_plot <- ggplot(dataBush, aes(Days, UniqueWordsNormalised))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWordsNormalised") + geom_smooth(method="lm")
-#jpeg("~/Documents/NLP/plots/GHWBUniqueWords.jpg", width = 350, height = 350)
 scatter_plot <- ggplot(dataBush, aes(Days, UniqueWordsNormalised))
 scatter_plot + geom_point() + labs(x = "Days", y = "UniqueWordsNormalised") + geom_smooth(method="loess")
 cor.test(dataBush$Days, dataBush$UniqueWords, method = "pearson", conf.level = 0.95)
-#dev.off()
-
 
 scatter_plot <- ggplot(dataReagan, aes(Days, social))
 scatter_plot + geom_point() + labs(x = "Days", y = "social") + geom_smooth(method="loess")
@@ -234,30 +223,7 @@ sd(dataTrump$MLU)
 loessTest = loess(ppron ~ Days, data=dataReagan)
 res = loessTest$residuals
 sse = sum(res^2)
-plot(loessTest)
 
-gamModel1 = gam(ppron ~ s(Days, bs="gp"), data = dataReagan, family = Gamma(link="log"), method="REML")
+gam_mod = gam(ppron ~ s(Days), data = dataReagan )
 coef(gam_mod)
 plot(gam_mod, residuals = TRUE, pch = 1)
-
-# Sum of squares as assessment of fit? If so, how to calculate?
-# Can I do glm with multiple predictors? How does this work?
-# Draw all two points approximately 2 years apart from the first 2 data points.
-# Taking this apart
-
-
-model1 = glm(formula = dataReagan$ppron ~ dataReagan$Days, family = gaussian)
-summary(model1)
-plot(model1)
-
-model2 = glm(formula = dataReagan$ppron ~ dataReagan$Days,
-             family = Gamma(link = "identity"))
-summary(model2)
-plot(model2)
-
-
-
-model3 = glm(formula = dataReagan$ppron ~ dataReagan$Days,
-             family = Gamma(link = "log"))
-summary(model3)
-plot(model3)
