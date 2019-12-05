ff03ebfd71ad1a08f15c07958a6a08bbcf1d3c3a
Joe Alcantara <joe@joealcantara.com>
2019-12-05 12:24:24 +0000

    created authors.R

---
+++ Authors/Authors.R
@@ -0,0 +1,26 @@
+# Clear Workspace
+rm(list=ls())
+
+# Load Data
+PDJ <- read.csv("~/Documents/NLP/Authors/PDJ.csv")
+AC <- read.csv("~/Documents/NLP/Authors/AC.csv")
+IM <- read.csv("~/Documents/NLP/Authors/IM.csv")
+
+
+scatter_plot <- ggplot(AC, aes(Year.of.Publication , TTR))
+scatter_plot +  geom_point() + labs(x = "Year", y = "ppron") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
+
+scatter_plot <- ggplot(IM, aes(Year.of.Publication , TTR))
+scatter_plot +  geom_point() + labs(x = "Year", y = "ppron") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
+
+scatter_plot <- ggplot(PDJ, aes(Year.of.Publication , TTR))
+scatter_plot +  geom_point() + labs(x = "Year", y = "ppron") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
+
+scatter_plot <- ggplot(AC, aes(Year.of.Publication , UniqueStems))
+scatter_plot +  geom_point() + labs(x = "Year", y = "UniqueStems") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
+
+scatter_plot <- ggplot(IM, aes(Year.of.Publication , UniqueStems))
+scatter_plot +  geom_point() + labs(x = "Year", y = "UniqueStems") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
+
+scatter_plot <- ggplot(PDJ, aes(Year.of.Publication , UniqueStems))
+scatter_plot +  geom_point() + labs(x = "Year", y = "UniqueStems") + geom_smooth(method="lm", color = "black", lty = 1) + geom_smooth(method="loess", color = "black", lty = 2) + theme_gray()
